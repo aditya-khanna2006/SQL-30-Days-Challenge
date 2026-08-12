0271d4a6bd3c0b69bcea75ed14d5ece58e21252f
Aditya Khanna <adityakhanna747@gmail.com>
2026-08-12 23:44:53 +0530

    Add files via upload

---
+++ Day-15 Comparison Operator/Day 15 Comparision Operator.sql
@@ -0,0 +1,14 @@
+-- Comparision Operator
+select * from employee2;
+
+-- age = 30
+select * from employee2
+where age = 30;
+
+-- age other than 30
+select * from employee2
+where age <> 30;
+
+-- Salary greater than 50000
+select * from employee2
+where salary > 50000;
