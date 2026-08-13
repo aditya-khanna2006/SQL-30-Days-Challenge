049452112e25fcbc15b77f27c86eacd26159a461
Aditya Khanna <adityakhanna747@gmail.com>
2026-08-13 09:32:54 +0530

    Add files via upload

---
+++ Day-20-Aggregate Functions/Day 20 Aggregate Functions.sql
@@ -0,0 +1,73 @@
+DROP TABLE IF EXISTS products;
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price NUMERIC(10, 2),
+    quantity INT,
+    added_date DATE,
+    discount_rate NUMERIC(5, 2)
+);
+
+INSERT INTO products (product_name, category, price, quantity, added_date, discount_rate) VALUES
+('Laptop', 'Electronics', 75000.50, 10, '2024-01-15', 10.00),
+('Smartphone', 'Electronics', 45000.99, 25, '2024-02-20', 5.00),
+('Headphones', 'Accessories', 1500.75, 50, '2024-03-05', 15.00),
+('Office Chair', 'Furniture', 5500.00, 20, '2023-12-01', 20.00),
+('Desk', 'Furniture', 8000.00, 15, '2023-11-20', 12.00),
+('Monitor', 'Electronics', 12000.00, 8, '2024-01-10', 8.00),
+('Printer', 'Electronics', 9500.50, 5, '2024-02-01', 7.50),
+('Mouse', 'Accessories', 750.00, 40, '2024-03-18', 10.00),
+('Keyboard', 'Accessories', 1250.00, 35, '2024-03-18', 10.00),
+('Tablet', 'Electronics', 30000.00, 12, '2024-02-28', 5.00);
+
+
+SELECT * FROM products;
+
+-- Sum --> Total Quantity Available of all products
+select sum(quantity) as total_quantity from products;
+
+-- Count --> Total number of products
+select count(*) as total_product from products;
+
+-- count with condition
+
+SELECT COUNT(*) AS total_products
+FROM products
+WHERE product_name LIKE '%phone%';
+
+-- Avg --> Average Price of Products
+select avg(price) as average_price from products;
+
+-- Average Price of Products with condition
+SELECT AVG(price) AS average_price
+FROM products
+WHERE category='Accessories' OR added_date > '2024-02-01';
+
+-- Maximum and Minimum price
+select max(price) as maximum_price,
+	   min(price) as minimum_price
+from products;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
